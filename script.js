c9adde02a3f85c03c4f1b6b05350eacb83d51087
Thales Barreto <trvbarreto@gmail.com>
2022-08-25 12:12:34 -0300

    Add playRound function for playing a single round of the game

--- script.js
+++ script.js
@@ -7,5 +7,30 @@ function getComputerChoice() {
     console.log(choices[random]);
 }
 
-getComputerChoice()
-getComputerChoice()
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+
+    if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        console.log("You Win! Rock beats Paper"); 
+    } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+        console.log("You Lose! Paper beats Rock"); 
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        console.log("You Win! Paper beats "); 
+    } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+        console.log("You Lose! Scissors beats Paper"); 
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        console.log("You Win! Scissors beats Paper"); 
+    } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+        console.log("You Lose! Rock beats Scissors"); 
+    } else {
+        console.log("It's a tie!"); 
+    }
+}
+
+
+// Tests
+playRound("ROCK", "Scissors")
+playRound("Paper", "scissors")
+playRound("ScIsSorS", "Scissors")
