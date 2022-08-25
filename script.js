92670b7a39c892644afe2792be9767e03dad9381
Thales Barreto <trvbarreto@gmail.com>
2022-08-25 11:59:09 -0300

    Add getComputerChoice function to randomly return an option

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+
+    let choices = ["Rock", "Paper", "Scissors"];
+
+    let random = Math.floor(Math.random() * 3);
+
+    console.log(choices[random]);
+}
+
+getComputerChoice()
+getComputerChoice()
