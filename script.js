7b18073bd819633daad097323b63a7812f55891d
Thales Barreto <trvbarreto@gmail.com>
2022-09-01 12:15:39 -0300

    Add checkWinner() for announcing a winner of the game once one player reaches 5 points.
    This function in set to run after each point incremention

--- script.js
+++ script.js
@@ -27,24 +27,44 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
     if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
         results.textContent = "You Win! Rock beats Scissors";
         playerPoints++;
+        checkWinner();
     } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
         results.textContent = "You Lose. Paper beats Rock";
         computerPoints++;
+        checkWinner();
     } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
         results.textContent = "You Win! Paper beats Rock"; 
         playerPoints++;
+        checkWinner();
     } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
         results.textContent = "You Lose. Scissors beats Paper"; 
         computerPoints++;
+        checkWinner();
     } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
         results.textContent = "You Win! Scissors beats Paper";
         playerPoints++;
+        checkWinner();
     } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
         results.textContent = "You Lose. Rock beats Scissors"; 
         computerPoints++;
+        checkWinner();
     } else {
         results.textContent = "It is a tie!"; 
     }
+}
+
+function checkWinner() {
+    if (playerPoints === 5) {
+        console.log(`Player Wins! - Score: - ${playerPoints} x ${computerPoints}`);
+        playerPoints = 0;
+        computerPoints = 0;
+    }
+    
+    if (computerPoints === 5) {
+        console.log(`Computer Wins! - Score: - ${computerPoints} x ${playerPoints}`);
+        playerPoints = 0;
+        computerPoints = 0;
+    } 
 }
 
 const buttons = document.querySelectorAll('button');
